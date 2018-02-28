7d1f0a9b047548ddb2aac99f68b953cd6b490590
Devin Collier <devinlcollier@gmail.com>
2018-02-28 15:15:04 -0700

    Final touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,10 +7,10 @@ function randInt(min, max)
 
 function arrayShuffle(array) 
 {
-	for(var i = array.length; i > 0; i--)
+	for(let i = array.length; i > 0; i--)
 	{
-		var r = randInt(0, i);
-		var temp = array[i];
+		let r = randInt(0, i);
+		let temp = array[i];
 		array[i] = array[r];
 		array[r] = temp;
 	}
@@ -29,28 +29,28 @@ class question{
 		this.element = $("<div>");
 		this.element.addClass("col-lg-8 col-lg-offset-2");
 
-		var question = $("<h2>");
+		let question = $("<h2>");
 		question.addClass("text-box");
 		question.text(this.question_text);
 
 		this.element.append(question);
 		this.answersE = [];
 
-		var answerE = $("<div>");
+		let answerE = $("<div>");
 		answerE.addClass("row");
 		answerE.append($("<div>").addClass("col-lg-12"));
-		answerE.children().append($("<p>").text(this.answer));
-		answerE.children().children().addClass("question-box");
+		answerE.children().append($("<button>").text(this.answer));
+		answerE.children().children().addClass("question-box btn btn-info center-block");
 		answerE.on("click", this.click);
 		this.answersE.push(answerE);
 
-		for (var i = 0; i < this.answers_array.length; i++) 
+		for (let i = 0; i < this.answers_array.length; i++) 
 		{
 		    answerE = $("<div>");
 		    answerE.addClass("row");
 		    answerE.append($("<div>").addClass("col-lg-12"));
-		    answerE.children().append($("<p>").text(this.answers_array[i]));
-		    answerE.children().children().addClass("question-box");
+		    answerE.children().append($("<button>").text(this.answers_array[i]));
+		    answerE.children().children().addClass("question-box btn btn-info center-block");
 		    answerE.on("click", this.click);
 		    this.answersE.push(answerE);
 		}
@@ -63,9 +63,9 @@ class question{
 
 	show(parent)
 	{
-		var temp = this.element;
+		let temp = this.element;
 		this.answersE = arrayShuffle(this.answersE);
-		for(var i = 0; i < this.answersE.length; i++)
+		for(let i = 0; i < this.answersE.length; i++)
 		{
 			temp.append(this.answersE[i]);
 		}
@@ -73,13 +73,19 @@ class question{
 		parent.append(temp);
 	}
 
-	showAnswer(parrent)
+	showAnswer(parent)
 	{
+		parent.contents().detach();
+		let temp = $("<div>");
+		temp.addClass("col-lg-12");
+		temp.append($("<h4>").text("Correct Answer: " + this.answer).addClass("text-center"));
+		temp.append($("<img>").attr("src", this.answer_pic).addClass("answer-image img-responsive center-block"));
 
+		parent.append(temp);
 	}
 }
 
-var game = {
+const game = {
     questions: [],
     current_question: -1,
     score: 0,
@@ -91,10 +97,11 @@ var game = {
     {
         game.start_button.hide();
         game.score = 0;
-        for (var i = game.questions.length - 1; i > 0; i--)
+        game.current_question = -1;
+        for (let i = game.questions.length - 1; i > 0; i--)
         {
-            var r = randInt(0, i);
-            var temp = game.questions[i];
+            let r = randInt(0, i);
+            let temp = game.questions[i];
             game.questions[i] = game.questions[r];
             game.questions[r] = temp;
         }
@@ -103,13 +110,13 @@ var game = {
 
     nextQuestion: function()
     {
-        $("#question").empty();
+        $("#question").contents().detach();
         game.current_question++;
 		clearInterval(game.timer);
         if(game.current_question === game.questions.length)
         {
         	game.start_button.show();
-        	$("#question").append("<p class=\"text-center\"> Game Over! </p>");
+        	$("#question").append("<h4 class=\"text-center\"> Game Over! </h4>");
         	return;
         }
         
@@ -125,6 +132,12 @@ var game = {
         	game.score++;
         	$("#score").text("Score: " + game.score);
             game.questions[game.current_question].showAnswer($("#question"));
+            game.time_remaining = 5;
+        }
+        else
+        {
+        	game.questions[game.current_question].showAnswer($("#question"));
+            game.time_remaining = 5;
         }
     },
 
@@ -139,13 +152,13 @@ var game = {
     }
 };
 
-const path = "assets/images/";
-
 window.onload = function(){
 	game.start_button = $("#start_button");
 	game.start_button.on("click", game.newGame);
 
-	game.questions.push(new question("sample question", "correct", ["incorrect0", "incorrect1", "incorrect2"], null));
-	game.questions.push(new question("sample question2", "correct", ["incorrect0", "incorrect1", "incorrect2"], null));
-	game.questions.push(new question("sample question3", "correct", ["incorrect0", "incorrect1", "incorrect2"], null));
+	game.questions.push(new question("What year did America land on the Moon?", "1969", ["1970", "Fake News!", "1962"], "assets/images/on_the_moon.jpg"));
+	game.questions.push(new question("What is the first car in space?", "Elon Musk's personal Tesla Roadster", ["Bill Gate's personal Toyota Camry", "Fake News!", "Arnold Schwarzenegger's personal Hummer"], "assets/images/space_man.webp"));
+	game.questions.push(new question("What is the first artificial satellite?", "Sputnik 1", ["The Hubble Telescope", "Fake News!", "Elon Musk's personal Tesla Roadster"], "assets/images/Sputnik_1.jpg"));
+	game.questions.push(new question("Who was the first person to walk on the moon?", "Neil Armstrong", ["Edwin \"Buzz\" Aldrin", "Fake News!", "Al Gore"], "assets/images/neil_armtstrong.jpg"));
+
 }
